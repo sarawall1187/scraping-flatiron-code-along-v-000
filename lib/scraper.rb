829f27cc59f6ac5f35a7579e288326a10345abd3
Sara Walker <sarawall1187@gmail.com>
2019-01-18 16:44:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,9 @@ class Scraper
   
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    binding.pry
+    doc.css(".post").each do |post|
+      course = Course.new
+      course.title = 
   end
   
     def print_courses
